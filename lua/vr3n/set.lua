527e89a35e1b5e5775b0d1891fc809222aa40240
Vr3n <vickyspatel@gmail.com>
2022-11-09 23:31:42 +0530

    Installed Emmet.

--- lua/vr3n/set.lua
+++ lua/vr3n/set.lua
@@ -205,3 +205,38 @@ require("lspconfig")['intelephense'].setup({
     capabilities = capabilities,
 })
 
+local lspconfig = require("lspconfig")
+local configs = require('lspconfig.configs')
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+if not configs.ls_emmet then
+  configs.ls_emmet = {
+    default_config = {
+      cmd = { 'ls_emmet', '--stdio' };
+      filetypes = {
+        'html',
+        'css',
+        'scss',
+        'javascriptreact',
+        'typescriptreact',
+        'haml',
+        'xml',
+        'xsl',
+        'pug',
+        'slim',
+        'sass',
+        'stylus',
+        'less',
+        'sss',
+        'hbs',
+        'handlebars',
+      };
+      root_dir = function(fname)
+        return vim.loop.cwd()
+      end;
+      settings = {};
+    };
+  }
+end
+
+lspconfig.ls_emmet.setup { capabilities = capabilities }
