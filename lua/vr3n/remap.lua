dbb61ef02d2e3e06dd8d674b6789dbf30d6daa0f
vr3n <vickyspatel@gmail.com>
2022-11-25 12:58:35 +0530

    Python Dap Configured ✅

--- lua/vr3n/remap.lua
+++ lua/vr3n/remap.lua
@@ -62,3 +62,17 @@ nnoremap("<leader>fr", "<cmd>lua vim.lsp.buf.format({ timeout_ms = 10000 })<cr>"
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
+-- DAP Keymaps.
+vim.keymap.set("n", "<F5>", "<cmd>lua require'dap'.continue()<CR>")
+vim.keymap.set("n", "<F10>", "<cmd>lua require'dap'.step_over()<CR>")
+vim.keymap.set("n", "<F11>", "<cmd>lua require'dap'.step_into()<CR>")
+vim.keymap.set("n", "<F12>", "<cmd>lua require'dap'.step_out()<CR>")
+vim.keymap.set("n", "<leader>b", "<cmd>lua require'dap'.toggle_breakpoint()<CR>")
+vim.keymap.set("n", "<leader>B", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+vim.keymap.set(
+	"n",
+	"<leader>lp",
+	"<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>"
+)
+vim.keymap.set("n", "<leader>dr", "<cmd>lua require'dap'.repl.open()<CR>")
