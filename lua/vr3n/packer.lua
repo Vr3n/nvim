1d825a6def1d0230729c36251cd676e2732f823f
Vr3n <vickyspatel@gmail.com>
2023-01-10 20:24:43 +0530

    Added Harpoon 🥥

--- lua/vr3n/packer.lua
+++ lua/vr3n/packer.lua
@@ -27,6 +27,7 @@ return require("packer").startup(function(use)
 	-- Telescope (Navigation)
 	use({ "nvim-telescope/telescope.nvim", tag = "0.1.0", requires = { { "nvim-lua/plenary.nvim" } } })
 	use({ "BurntSushi/ripgrep" })
+	use("ThePrimeagen/harpoon")
 
 	use({ "kyazdani42/nvim-web-devicons" })
 
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -119,6 +119,11 @@ _G.packer_plugins = {
     path = "/home/vr3n/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
+  harpoon = {
+    loaded = true,
+    path = "/home/vr3n/.local/share/nvim/site/pack/packer/start/harpoon",
+    url = "https://github.com/ThePrimeagen/harpoon"
+  },
   ["indent-blankline.nvim"] = {
     loaded = true,
     path = "/home/vr3n/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
