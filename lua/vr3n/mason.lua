eefd90d8c1cefe889a9c220448f21b2cb53f0b09
Vr3n <vickyspatel@gmail.com>
2025-03-24 12:01:35 +0530

    flake8 to ruff

--- lua/vr3n/mason.lua
+++ lua/vr3n/mason.lua
@@ -1,54 +1,51 @@
 local status_ok, mason = pcall(require, "mason")
 
 if not status_ok then
-   return
+	return
 end
 
 local mason_lsp_config_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 
 if not mason_lsp_config_status then
-    return
+	return
 end
 
 local mason_nullls_config_status, mason_nullls = pcall(require, "mason-null-ls")
 
 if not mason_lsp_config_status then
-    return
+	return
 end
 
 mason.setup({
-    ensure_installed = {
-        "ts_ls",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "sumnekolua",
-        "pyright",
-        "djlint",
-        "jq",
-        "flake8",
-        "sql_formatter",
-    },
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+	ensure_installed = {
+		"ts_ls",
+		"html",
+		"cssls",
+		"tailwindcss",
+		"sumnekolua",
+		"pyright",
+		"djlint",
+		"jq",
+		"ruff",
+		"sql_formatter",
+	},
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
-
--- mason + lspconf 
+-- mason + lspconf
 mason_lspconfig.setup()
-
 
 -- mason + nullls
 mason_nullls.setup({
-    ensure_installed = {
-        "prettier",
-        "stylua",
-        "eslint",
-    }
+	ensure_installed = {
+		"prettier",
+		"stylua",
+		"eslint",
+	},
 })
-
--- lua/vr3n/null-ls.lua
+++ lua/vr3n/null-ls.lua
@@ -1,7 +1,7 @@
 local status_ok, configs = pcall(require, "null-ls")
 
 if not status_ok then
-    return
+	return
 end
 
 local formatting = configs.builtins.formatting
@@ -9,23 +9,23 @@ local diagnostics = configs.builtins.diagnostics
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 configs.setup({
-    sources = {
-        formatting.prettier,
-        formatting.stylua,
-        formatting.autopep8,
-        diagnostics.eslint_d,
-        diagnostics.flake8,
-    },
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ timeout_ms = 10000 })
-                end,
-            })
-        end
-    end,
+	sources = {
+		formatting.prettier,
+		formatting.stylua,
+		formatting.autopep8,
+		diagnostics.eslint_d,
+		diagnostics.ruff,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ timeout_ms = 10000 })
+				end,
+			})
+		end
+	end,
 })
